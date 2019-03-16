86de24becab2b107a9ff5fbf9eab85df8207614e
Skjamal <satinder.jamal@gmail.com>
2019-03-16 16:32:41 -0600

    Compiles successfully

--- src/main/scala/EchoServer.scala
+++ src/main/scala/EchoServer.scala
@@ -1,9 +1,18 @@
 
 import java.net._
 import java.io._
+
+
 import scala.io._
 
 object EchoServer {
+  def main(args: Array[String]) {
+    val server = new ServerSocket(9999)
+    while (true) {
+      serve(server)
+    }
+
+
   def serve(server: ServerSocket) { //the ServerSocket listens for client requests
     val s = server.accept() //the server accepts the clients connection
     val in = new BufferedReader(new InputStreamReader(s.getInputStream)) //val in = the data the client sends out to the server
@@ -13,7 +22,7 @@ object EchoServer {
 
     def read(in: BufferedReader): Unit = { //read method reads the inputstream and parses it to find the request (GET)
       val firstLine = in.readLine() //firstLIne captures the entire line including the ('\r\n')
-      var arrayOfFirstLineStrings = (firstLine.split("\\s+")) //this parses the first line by space and returns a list of strings
+    var arrayOfFirstLineStrings = (firstLine.split("\\s+")) //this parses the first line by space and returns a list of strings
 
       if (arrayOfFirstLineStrings(0) == "GET") { //if the first string in the array is the HTTP request "GET",
         read_next(arrayOfFirstLineStrings) //then send the array to the method read_next
@@ -25,28 +34,22 @@ object EchoServer {
 
     def read_next(arrayOfFirstLineStrings: Array[String]): Unit = { //read_next method
       val url = arrayOfFirstLineStrings(1) //the file name is contained within the second string in the array
-      val fileName = url.substring(1) //the url is the file name begins after the "/" in /index.html}
+    val fileName = url.substring(1) //the url is the file name begins after the "/" in /index.html}
       output(fileName) //
     }
 
     def output(fileName: String): Unit = {
-      if (fileName == null) {  //if file is not index.html then it will return null
+      if (fileName == null) { //if file is not index.html then it will return null
         out.write("HTTP/1.0 404 Not Found \r\n") //return 404 error
         out.write("\r\n")
       }
       else {
-        out.write("HTTP/1.0 200 Ok \r\n")  //if it index.html then return 200 response
+        out.write("HTTP/1.0 200 Ok \r\n") //if it index.html then return 200 response
         out.write("\r\n")
-      }
-
-    }
-    def main(args: Array[String]) {
-      val server = new ServerSocket(9999)
-      while (true) {
-        serve(server)
       }
       out.flush()
       s.close()
+    }
     }
   }
 }
